fb58440695b815bae66f53c6f7b64732b370e85a
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-08-07 03:55:23 +0600

    Inserted dummy data into database using  DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,72 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('contacts')->insert([
+
+            [
+                'name' => 'Harry Potter',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Charry Potter',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Prisoner of Azkaban',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Tom Bruce',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Cindrella Tabassum',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Naina Bose',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'The Shining Mina',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Raju Mia',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ],
+
+            [
+                'name' => 'Tomy Mia',
+                'email' => Str::random(10).'@example.com',
+                'phone' => random_int(1000000000,9999999999),
+                'address' => Str::random(50)
+            ]
+
+        ]);
+
     }
 }
